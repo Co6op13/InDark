c77a0e67e374db2515cf107dbeb2fa4fc7f083ca
Co6op13 <Co6op13@yandex.ru>
2023-05-19 12:40:59 +0500

    фальшвеер вроде готов

--- TopDawn/Assets/Falschfeuer.cs
+++ TopDawn/Assets/Falschfeuer.cs
@@ -5,47 +5,75 @@ using UnityEngine.Rendering.Universal;
 
 public class Falschfeuer : MonoBehaviour
 {
-    [SerializeField] private Light2D lightSpot;
-    [SerializeField] private CircleCollider2D collider2d;
-    [SerializeField] [Range (0,10)] private float minIntencyty, maxIntencyty;
-    [SerializeField] [Range(0, 10)] private float minInnerRadius, maxInnerRadius;
-    [SerializeField] [Range(0, 10)] private float minOuterRadius, maxOuterRadius;
+    [SerializeField] [Range (0,2)] private float minIntencyty, maxIntencyty;
+    [SerializeField] [Range(0, 2)] private float minInnerRadius, maxInnerRadius;
+    [SerializeField] [Range(0, 20)] private float minOuterRadius, maxOuterRadius;
     [SerializeField] private float lifeTime, decaylifeTime;
-    
+    [SerializeField] private float ratioLerp;
+    [SerializeField] [Range(0, 0.02f)] private float decay;
+    private Light2D lightSpot;
+    private CircleCollider2D collider2d;
+    [Space]
     private float time;
-    
-    void Start()
+    [SerializeField] private float timeHasPassed;
+
+    void Awake()
     {
-        //lightSpot = GetComponent<Light2D>();
+        lightSpot = GetComponent<Light2D>();
         collider2d = GetComponent<CircleCollider2D>();
-        time = Time.time + lifeTime;
-        StartCoroutine(LifeTime());
     }
 
+    private void OnEnable()
+    {
+        StartCoroutine(LifeTime());
+        Invoke("Stop", lifeTime + decaylifeTime);
+    }
+
+    private void Stop()
+    {
+        StopCoroutine(LifeTime());
+    }
 
     private IEnumerator LifeTime()
     {
-        while (time > Time.time)
+        collider2d.enabled = true;
+        lightSpot.enabled = true;
+        timeHasPassed = 0f;
+        bool check = true;
+        var delayTime = Time.fixedDeltaTime * ratioLerp;
+
+        while (check)
         {
-            lightSpot.intensity =Random.Range(minIntencyty, maxIntencyty);
-            lightSpot.pointLightInnerRadius = Random.Range(minInnerRadius, maxInnerRadius);
-            lightSpot.pointLightOuterAngle = Random.Range(minOuterRadius, maxOuterRadius);
-            yield return new WaitForSeconds(Time.fixedDeltaTime);
+            SetRandovVariableInLight();
+            yield return new WaitForSeconds(delayTime);            
+            timeHasPassed += delayTime;
+            check = timeHasPassed < lifeTime ? true : false;
         }
-        time = Time.time + decaylifeTime;
-        collider2d.enabled = false;
+
+        delayTime = Time.fixedDeltaTime * ratioLerp;
         lightSpot.intensity = maxIntencyty;
-        yield return new WaitForSeconds(Time.fixedDeltaTime * 2);
-        while (time > Time.time)
-        {
-            lightSpot.intensity -= Time.deltaTime;
-            yield return new WaitForSeconds(Time.fixedDeltaTime);
-        }
-        StopCoroutine(LifeTime());
-    }
-    // Update is called once per frame
-    void Update()
-    {
+        yield return new WaitForSeconds(delayTime);
+        collider2d.enabled = false;
+        timeHasPassed = 0f;
+        check = true;
         
+        while (check)
+        {
+            lightSpot.intensity -= decay;
+            lightSpot.pointLightOuterRadius -= decay * 10;
+            yield return new WaitForSeconds(delayTime);
+            timeHasPassed += delayTime;
+            check = timeHasPassed < decaylifeTime ? true : false;
+        }
+        lightSpot.enabled = false;       
+    }
+
+
+    private void SetRandovVariableInLight()
+    {
+        lightSpot.intensity = Random.Range(minIntencyty, maxIntencyty);
+        lightSpot.pointLightInnerRadius = Random.Range(minInnerRadius, maxInnerRadius);
+        // lightSpot.pointLightOuterRadius = 20f;
+        lightSpot.pointLightOuterRadius = Random.Range(minOuterRadius, maxOuterRadius);
     }
 }
